288b41e6ae5602d38457aadd874159c0afed1a6f
Maider <maiderbarrutia@hotmail.com>
2023-09-07 15:52:04 +0200

    Add exercise Module 2 - Tipos de datos y operadores

---
+++ 2-tipos-de-datos-y-operadores/src/main.ts
@@ -0,0 +1,15 @@
+import "./style.css";
+
+//OPCIÓN 1
+console.log(
+  "Resultado 1: " +
+    ((120 - 18) / 7).toFixed(2) +
+    "€ tiene que pagar cada uno por la cena, incluyendo el cumpleañero que tendrá que pagar su parte de la cena + los 18€ de las bebidas"
+);
+
+//OPCIÓN 2
+console.log(
+  "Resultado 2: " +
+    (120 - 18) / 6 +
+    "€ tiene que pagar cada uno por la cena si invitan al cumpleañero 😂"
+);
