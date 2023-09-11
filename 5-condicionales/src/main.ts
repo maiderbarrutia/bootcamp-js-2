d28c8d9b0196ac1c021925eabc91f68f9546733e
Maider <maiderbarrutia@hotmail.com>
2023-09-11 21:16:54 +0200

    Code refactoring in exercise Module 5

--- 5-condicionales/src/main.ts
+++ 5-condicionales/src/main.ts
@@ -1,25 +1,10 @@
 import "./style.css";
 
 //VARIABLES GENERALES
-const giveCardButton = document.getElementById(
-  "giveCardButton"
-) as HTMLButtonElement;
-const cardImg = document.getElementById("cardImg") as HTMLImageElement;
-const messagesContent = document.getElementById(
-  "messagesContent"
-) as HTMLParagraphElement;
-const restartGameButton = document.getElementById(
-  "restartButton"
-) as HTMLDivElement;
-const standUpButton = document.getElementById(
-  "standUpButton"
-) as HTMLButtonElement;
-const cardImgContainer = document.querySelector(
-  ".cardAndMessage"
-) as HTMLDivElement;
-const whatHappensButton = document.getElementById(
-  "whatHappensButton"
-) as HTMLButtonElement;
+const giveCardButton = document.getElementById("giveCardButton");
+const restartGameButton = document.getElementById("restartButton");
+const whatHappensButton = document.getElementById("whatHappensButton");
+const standUpButton = document.getElementById("standUpButton");
 
 let score: number = 0;
 
@@ -27,162 +12,232 @@ let score: number = 0;
 function showScore(): void {
   const showScore = document.getElementById("showScore");
   if (showScore !== null && showScore !== undefined) {
-    showScore.innerHTML = score.toString();
+    showScore.textContent = score.toString();
   }
 }
-document.addEventListener("DOMContentLoaded", () => {
-  showScore();
-});
 
-//PEDIR CARTA: Generar carta aleatoria
-function giveCard(): void {
-  const chosenCard = Math.ceil(Math.random() * 12);
-  if (chosenCard <= 7 || chosenCard >= 10) {
-    showCard(chosenCard);
-    cardScore(chosenCard);
-  }
-}
-if (giveCardButton !== null && giveCardButton !== undefined) {
-  giveCardButton.addEventListener("click", giveCard);
-}
+//GENERAR NUMERO ALEATORIO
+const generateRandomNumber = (): number => Math.ceil(Math.random() * 10);
 
-// MOSTRAR CARTA
-const showCard = (carta: number): void => {
-  switch (carta) {
+//GENERAR NÚMERO DE CARTA
+const generateCardNumber = (cardNumber: number): number =>
+  cardNumber <= 7 ? cardNumber : cardNumber + 2;
+
+//ASIGNAR URL CON LA IMAGEN DE CADA CARTA A CADA NÚMERO
+const getCardUrl = (card: number): string => {
+  let cardUrl: string = "";
+  switch (card) {
     case 1:
-      cardImg.src =
+      cardUrl =
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
       break;
 
     case 2:
-      cardImg.src =
+      cardUrl =
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
       break;
 
     case 3:
-      cardImg.src =
+      cardUrl =
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
       break;
 
     case 4:
-      cardImg.src =
+      cardUrl =
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
       break;
 
     case 5:
-      cardImg.src =
+      cardUrl =
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
       break;
 
     case 6:
-      cardImg.src =
+      cardUrl =
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
       break;
 
     case 7:
-      cardImg.src =
+      cardUrl =
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
       break;
 
     case 10:
-      cardImg.src =
+      cardUrl =
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
       break;
 
     case 11:
-      cardImg.src =
+      cardUrl =
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
       break;
 
     case 12:
-      cardImg.src =
+      cardUrl =
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
       break;
 
     default:
+      cardUrl =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
       break;
   }
+  return cardUrl;
 };
 
-//SUMAR PUNTUACIÓN Y MOSTRARLA
-const cardScore = (chosenCard: number): void => {
+// PUNTOS DE CADA CARTA
+const cardPoints = (cardNumber: number): void => {
   let points;
-  if (chosenCard === 10 || chosenCard === 11 || chosenCard === 12) {
-    points = 0.5;
-  } else {
-    points = chosenCard;
-  }
+  cardNumber === 10 || cardNumber === 11 || cardNumber === 12
+    ? (points = 0.5)
+    : (points = cardNumber);
+  totalPoints(points);
+};
+
+//PUNTUACIÓN TOTAL
+function totalPoints(points: number) {
   score = score + points;
   showScore();
+  winOrLose(score);
+}
 
-  if (score > 7.5) {
+//PINTAR LA CARTA MEDIANTE URL SEGUN NUMERO DE CARTA
+function assignCardUrl(card: number): void {
+  const urlCarta = getCardUrl(card);
+  const imgCard = document.getElementById("cardImg");
+
+  if (
+    imgCard !== undefined &&
+    imgCard !== null &&
+    imgCard instanceof HTMLImageElement
+  ) {
+    imgCard.src = urlCarta;
+  }
+}
+
+// DAR CARTA
+function giveCard(): void {
+  const randomNumber = generateRandomNumber();
+  const cardNumber = generateCardNumber(randomNumber);
+
+  assignCardUrl(cardNumber);
+  cardPoints(cardNumber);
+}
+
+// HAS GANADO O PERDIDO?
+function winOrLose(totalScore: number) {
+  if (totalScore > 7.5) {
     gameOver();
   }
+  if (totalScore === 7.5) {
+    win();
+  }
+}
+
+//MENSAJES
+function messages(messageText: string, color: string): void {
+  const messagesContent = document.getElementById("messagesContent");
+  if (
+    messagesContent !== null &&
+    messagesContent !== undefined &&
+    messagesContent instanceof HTMLParagraphElement
+  ) {
+    messagesContent.textContent = messageText;
+    messagesContent.style.backgroundColor = color;
+  }
+}
+
+//HABILITAR BOTONES
+const enableOrDisableButton = (id: string, enabled: boolean): void => {
+  const button = document.getElementById(id);
+  button !== null && button !== undefined && button instanceof HTMLButtonElement
+    ? (button.disabled = enabled)
+    : console.error(`No existe el <button> con id: ${id}`);
 };
 
-//TERMINAR PARTIDA CUANDO SUPERAS 7.5 PUNTOS
+//INICIAR JUEGO
+function restartGame() {
+  score = 0;
+  showScore();
+  enableOrDisableButton("giveCardButton", false);
+  enableOrDisableButton("standUpButton", false);
+  enableOrDisableButton("restartButton", true);
+  enableOrDisableButton("whatHappensButton", true);
+  assignCardUrl(0);
+  messages("", "");
+}
+
+// JUEGO TERMINADO
 const gameOver = (): void => {
-  giveCardButton.style.display = "none";
-  cardImgContainer.classList.add("blocked");
-  messagesContent.innerHTML = "GameOver";
-  messagesContent.style.backgroundColor = "red";
-  // cardImg.style.display = "none";
-  cardImg.src =
-    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
-  restartGameButton.style.display = "flex";
-  standUpButton.style.display = "none";
+  messages("game over", "red");
+  enableOrDisableButton("giveCardButton", true);
+  enableOrDisableButton("standUpButton", true);
+  enableOrDisableButton("restartButton", false);
+  enableOrDisableButton("whatHappensButton", true);
+  assignCardUrl(0);
 };
 
 //PLANTARSE
 function stopGame() {
-  giveCardButton.style.display = "none";
-  standUpButton.style.display = "none";
-  restartGameButton.style.display = "flex";
-  messagesContent.style.backgroundColor = "green";
-  whatHappensButton.style.display = "flex";
-  cardImgContainer.classList.add("blocked");
-  if (score <= 4) {
-    messagesContent.innerHTML = "Has sido muy conservador";
-  } else if (score > 4 && score < 6) {
-    messagesContent.innerHTML = "Te ha entrado el canguelo eh?";
-  } else if (score >= 6 && score <= 7) {
-    messagesContent.innerHTML = "Casi casi...";
-  } else if (score === 7.5) {
-    messagesContent.innerHTML = "¡ Lo has clavado! ¡Enhorabuena!";
-  }
-}
-if (standUpButton !== null && standUpButton !== undefined) {
-  standUpButton.addEventListener("click", stopGame);
+  enableOrDisableButton("giveCardButton", true);
+  enableOrDisableButton("standUpButton", true);
+  enableOrDisableButton("restartButton", false);
+  enableOrDisableButton("whatHappensButton", false);
+  stopGameMessages();
 }
 
-// REINICIAR JUEGO
-function restartGame() {
-  score = 0;
-  showScore();
-  cardImgContainer.classList.remove("blocked");
-  messagesContent.style.backgroundColor = "";
-  cardImg.style.display = "flex";
-  whatHappensButton.style.display = "none";
-  cardImg.src =
-    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
-  giveCardButton.style.display = "flex";
-  messagesContent.innerHTML = "";
-  restartGameButton.style.display = "none";
-  standUpButton.style.display = "flex";
+//HAS GANADO LA PARTIDA CON 7.5 PUNTOS
+function win() {
+  enableOrDisableButton("giveCardButton", true);
+  enableOrDisableButton("standUpButton", true);
+  enableOrDisableButton("restartButton", false);
+  enableOrDisableButton("whatHappensButton", true);
+  stopGameMessages();
 }
-if (restartGameButton !== null && restartGameButton !== undefined) {
-  restartGameButton.addEventListener("click", restartGame);
+
+// MENSAJES AL PLANTARSE
+function stopGameMessages() {
+  if (score <= 4) {
+    messages("Has sido muy conservador", "green");
+  } else if (score > 4 && score < 6) {
+    messages("Te ha entrado el canguelo eh?", "green");
+  } else if (score >= 6 && score <= 7) {
+    messages("Casi casi...", "green");
+  } else if (score === 7.5) {
+    messages("¡Lo has clavado! ¡Enhorabuena!", "green");
+  }
 }
 
 // SABER LO QUE HABRÍA PASADO
 function guessWhatHappens() {
-  cardImgContainer.classList.remove("blocked");
-  messagesContent.style.backgroundColor = "";
-  whatHappensButton.style.display = "none";
-  messagesContent.innerHTML = "";
-  giveCardButton.style.display = "flex";
+  enableOrDisableButton("giveCardButton", false);
+  enableOrDisableButton("standUpButton", false);
+  enableOrDisableButton("restartButton", true);
+  enableOrDisableButton("whatHappensButton", true);
+
+  messages("", "");
 }
 
-if (whatHappensButton !== null && whatHappensButton !== undefined) {
-  whatHappensButton.addEventListener("click", guessWhatHappens);
+//BOTONES
+function buttons(button: HTMLElement | null, handler: () => void) {
+  if (
+    button instanceof HTMLButtonElement &&
+    button !== undefined &&
+    button !== null
+  ) {
+    button.addEventListener("click", handler);
+  }
 }
+
+//INICIAR JUEGO
+function loadGame() {
+  buttons(giveCardButton, giveCard);
+  buttons(restartGameButton, restartGame);
+  buttons(whatHappensButton, guessWhatHappens);
+  buttons(standUpButton, stopGame);
+}
+
+/* -------------------- CARGAR PARTIDA -------------*/
+document.addEventListener("DOMContentLoaded", () => {
+  loadGame();
+});
